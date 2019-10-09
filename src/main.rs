f6ade2ea31c16a8397cfb904cb744cad5f6d3784
Eli Lipsitz <eli.lipsitz@gmail.com>
2019-10-08 23:49:24 -0700

    Basic command line argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} [path to rom file]", args[0]);
+        process::exit(1);
+    }
+
+    let rom: &str = &args[1];
+    println!("Loading rom: {}", rom);
 }
