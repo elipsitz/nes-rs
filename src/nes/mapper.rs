c3229a1fbd075e9cce8196bdac9d6cd4cb7795b6
Eli Lipsitz <eli.lipsitz@gmail.com>
2020-05-22 15:18:38 -0500

    mapper: Finish implementing mapper 0

--- src/nes/mapper.rs
+++ src/nes/mapper.rs
@@ -7,18 +7,44 @@ pub trait Mapper {
 
 pub fn make_mapper(cart: Cartridge) -> Box<dyn Mapper> {
     match cart.mapper_id {
-        0 => Box::new(Mapper0 { cart }),
+        0 => Box::new(Mapper0::new(cart)),
         _ => panic!("Unknown mapper ID: {}", cart.mapper_id)
     }
 }
 
-pub struct Mapper0 {
-    cart: Cartridge
+struct Mapper0 {
+    cart: Cartridge,
+    vram: [u8; 2048],
+}
+
+impl Mapper0 {
+    fn translate_vram(&self, addr: u16) -> u16 {
+        if self.cart.mirror_mode == 0 {
+            // Horizontal mirroring.
+            (addr & 0x3FF) | ((addr & 0x800) >> 1)
+        } else {
+            // Vertical mirroring.
+            addr & 0x7FF
+        }
+    }
+
+    fn new(cart: Cartridge) -> Mapper0 {
+        assert!(cart.mirror_mode == 0 || cart.mirror_mode == 1);
+        Mapper0 {
+            cart,
+            vram: [0; 2048],
+        }
+    }
 }
 
 impl Mapper for Mapper0 {
     fn peek(&mut self, addr: u16) -> u8 {
         match addr {
+            // PPU
+            0x0000..=0x1FFF => self.cart.chr_rom[addr as usize],
+            0x2000..=0x3EFF => self.vram[self.translate_vram(addr) as usize],
+
+            // CPU
             0x8000..=0xFFFF => {
                 let offset = addr - 0x8000;
                 let size = self.cart.prg_rom.len() as u16;
@@ -28,6 +54,12 @@ impl Mapper for Mapper0 {
         }
     }
 
-    fn poke(&mut self, _addr: u16, _val: u8) {
+    fn poke(&mut self, addr: u16, val: u8) {
+        match addr {
+            // PPU
+            0x0000..=0x1FFF => self.cart.chr_rom[addr as usize] = val,
+            0x2000..=0x3EFF => self.vram[self.translate_vram(addr) as usize] = val,
+            _ => {}
+        };
     }
 }
