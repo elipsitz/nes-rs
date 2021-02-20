4a9eb5c3d029b42a46f1d9a6f38e21c8fcccdd3e
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-02-20 13:48:48 -0600

    wasm: play through emulator in real-time

--- nes_wasm/www/script.js
+++ nes_wasm/www/script.js
@@ -3,6 +3,11 @@ import init, * as nes from '../pkg/nes_wasm.js';
 const NES_WIDTH = 256;
 const NES_HEIGHT = 240;
 
+var emulator = null;
+var canvas = null;
+var canvas_ctx = null;
+var canvas_data = null;
+
 function openRom(event) {
     var input = event.target;
     
@@ -10,25 +15,33 @@ function openRom(event) {
     reader.onload = function(){
         var arrayBuffer = reader.result;
         var data = new Uint8Array(arrayBuffer);
-        var emulator = new nes.Emulator(data);
-        for (var i = 0; i < 60; i += 1) {
-            emulator.emulate_frame();
-        }
+        emulator = new nes.Emulator(data);
 
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        var data = ctx.createImageData(NES_WIDTH, NES_HEIGHT);
-        emulator.get_frame_buffer(data.data);
-        ctx.putImageData(data, 0, 0);
+        window.requestAnimationFrame(emulatorStep);
     };
     reader.readAsArrayBuffer(input.files[0]);
 }
 
-async function run() {
+function emulatorStep() {
+    emulator.emulate_frame();
+    emulator.get_frame_buffer(canvas_data.data);
+    canvas_ctx.putImageData(canvas_data, 0, 0);
+
+    window.requestAnimationFrame(emulatorStep);
+}
+
+async function onLoad() {
+    // Initialize wasm.
     await init();
 
+    // Initialize canvas.
+    canvas = document.getElementById("canvas");
+    canvas_ctx = canvas.getContext("2d");
+    canvas_data = canvas_ctx.createImageData(NES_WIDTH, NES_HEIGHT);
+
+    // Initialize ROM selector.
     let file_selector = document.getElementById("rom_input");
     file_selector.addEventListener("change", openRom);
 }
 
-run();
+window.addEventListener("load", onLoad);
